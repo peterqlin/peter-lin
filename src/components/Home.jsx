8d974b0fd28bae8350286d29f3c0cbac5f02739d
230pelin <peter.lin2305@gmail.com>
2025-07-01 17:49:14 -0400

    made homepage mobile friendly

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,14 +7,14 @@ function Home() {
     <section id="home" className="h-screen bg-blue-50 relative overflow-hidden">
       <div className="absolute inset-0 flex items-center justify-center py-4">
         {/* Single container that holds both text and image as one unit */}
-        <div className="flex items-center justify-center gap-32 max-w-6xl mx-auto px-8">
+        <div className="flex flex-col md:flex-row items-center justify-center gap-8 md:gap-32 max-w-6xl mx-auto px-4 md:px-8">
           {/* Text Section */}
-          <div className="text-gray-900 flex-shrink-0">
+          <div className="text-gray-900 flex-shrink-0 text-center md:text-left">
             <AnimatedText text={`Hi, I'm\nPeter Lin`} />
           </div>
 
-          {/* Image Section - smaller, more appropriate size */}
-          <div className="w-120 h-120 flex-shrink-0">
+          {/* Image Section - responsive sizing */}
+          <div className="w-64 h-64 md:w-96 md:h-96 lg:w-120 lg:h-120 flex-shrink-0">
             <PaperCanvas />
           </div>
         </div>
